18a832b1a0b45b7db47604ce6c3a081c9b821dde
shuvo4092 <mahdishuvo99@gmail.com>
2026-09-01 19:35:48 +0600

    update utils

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,13 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
 print("\nCalculator Results:")
 print("10 + 5 =", add(10, 5))
 print("10 - 5 =", subtract(10, 5))
+print("10 * 5 =", multiply(10, 5))
